ee15fb4ece9e17fe564451c024869cf62c440f43
bradyDarren <dcgbfinancial@gmail.com>
2024-12-26 16:54:35 -0600

    intital commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+import random 
+
+
+screen = Screen()
+"""#1 create a black background for the screen"""
+screen.bgcolor("black")
+"""#2 create a Score counter at the top of the screen"""
+screen.title("Welcome")
+"""#3 create a snake with a square body"""
+"""#4 randomize a circle that shows up to resemble food."""
+"""#5 If the snake comes into contact with the food make the snake grow."""
+
+
+
+
+screen.exitonclick()
