d6df1599a0a26ce147d2fcd1fed5797dc59b8398
bradyDarren <dcgbfinancial@gmail.com>
2024-12-27 08:33:49 -0600

    addition of the snake body

--- main.py
+++ main.py
@@ -16,8 +16,23 @@ score_display.goto(x=-10,y=550)
 score_display.color("white","white")
 score_display.write(f"Score: {score}", move=True,align="center",font=("Ariel",40,"normal"))
 
-"""#2 create a Score counter at the top of the screen"""
 """#3 create a snake with a square body"""
+snake_head = Turtle(shape="square")
+snake_head.penup()
+snake_head.color("white")
+print(snake_head.position())
+
+snake_body = Turtle(shape="square")
+snake_body.penup()
+snake_body.color("white")
+snake_body.goto(x=-20,y=0)
+print(snake_body.position())
+
+snake_tail = Turtle(shape="square")
+snake_tail.penup()
+snake_tail.color("white")
+snake_tail.goto(x=-40,y=0)
+print(snake_tail.position())
 
 """#4 randomize a circle that shows up to resemble food."""
 """#5 If the snake comes into contact with the food make the snake grow."""
