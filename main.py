a456e08699847744a3bef813e0c38d32ead39f4e
bradyDarren <dcgbfinancial@gmail.com>
2024-12-27 08:16:54 -0600

    finsihing of the screen and displaying of score

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import random
 screen = Screen()
 """#1 create a black background for the screen as well as the score count indicator."""
 screen.bgcolor("black")
+screen.setup(height=1250, width=1250)
+screen.title("Classic Snake")
 
 score = 0
 score_display = Turtle()
+score_display.hideturtle()
 score_display.penup()
+score_display.goto(x=-10,y=550)
 score_display.color("white","white")
-score_display.write(f"Score: {score}", move=True,align="center",font=("Ariel",20,"normal"))
+score_display.write(f"Score: {score}", move=True,align="center",font=("Ariel",40,"normal"))
 
 """#2 create a Score counter at the top of the screen"""
 """#3 create a snake with a square body"""
