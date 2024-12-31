27a33033923bc4984ce2a41778dc18219a60d229
bradyDarren <dcgbfinancial@gmail.com>
2024-12-31 14:10:49 -0600

    addition of the OOP code for the scoreboard class

--- scoreboard.py
+++ scoreboard.py
@@ -1,11 +1,18 @@
 from turtle import Turtle
 
 class Scoreboard(Turtle): 
-    def __init__(self):
+    def __init__(self, xcord, ycord):
+        super().__init__()
         self.score = 0
-        self.score_display = Turtle()
-    score_display.hideturtle()
-    score_display.penup()
-    score_display.goto(x=-10,y=450)
-    score_display.color("white","white")
-    score_display.write(f"Score: {score}",move=False,align="center",font=("Ariel",40,"normal"))
+        self.hideturtle()
+        self.penup()
+        self.goto(x=xcord,y=ycord)
+        self.color("white","white")
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}",move=False,align="center",font=("Ariel",40,"normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
