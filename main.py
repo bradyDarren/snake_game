96f942cd73a05d24fbd51f8285222a0009db9f0b
bradyDarren <dcgbfinancial@gmail.com>
2024-12-31 14:30:29 -0600

    adjustment for the scoreboard class for the ycord

--- main.py
+++ main.py
@@ -11,13 +11,10 @@ screen.setup(height=1000, width=1250)
 screen.title("Classic Snake")
 screen.tracer(0)
 
-# score = 0
-# score_display = Turtle()
-# score_display.hideturtle()
-# score_display.penup()
-# score_display.goto(x=-10,y=450)
-# score_display.color("white","white")
-# score_display.write(f"Score: {score}",move=False,align="center",font=("Ariel",40,"normal"))
+score_ycord = int(((screen.window_height()) / 2) - 50)
+
+score = Scoreboard(xcord=0, ycord=score_ycord)
+score.update_score()
 
 """#2 create a snake with a square body"""
 initial_ycord = 0
