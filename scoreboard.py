18ad342f0ee73de05eb45b5c619378434b90ce31
bradyDarren <dcgbfinancial@gmail.com>
2024-12-31 00:03:22 -0600

    refactorin

--- scoreboard.py
+++ scoreboard.py
@@ -1,8 +1,9 @@
 from turtle import Turtle
 
-class Scoreboard: 
-    score = 0
-    score_display = Turtle()
+class Scoreboard(Turtle): 
+    def __init__(self):
+        self.score = 0
+        self.score_display = Turtle()
     score_display.hideturtle()
     score_display.penup()
     score_display.goto(x=-10,y=450)
