e46bd4804b86cdfbf44a07a7e12887a0996e69b4
bradyDarren <dcgbfinancial@gmail.com>
2024-12-30 23:37:29 -0600

    initial commit

--- scoreboard.py
+++ scoreboard.py
@@ -1,2 +1,10 @@
+from turtle import Turtle
+
 class Scoreboard: 
-    
+    score = 0
+    score_display = Turtle()
+    score_display.hideturtle()
+    score_display.penup()
+    score_display.goto(x=-10,y=450)
+    score_display.color("white","white")
+    score_display.write(f"Score: {score}",move=False,align="center",font=("Ariel",40,"normal"))
