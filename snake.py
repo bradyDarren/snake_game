9d154e9f48c69c394f0ba91693e0eb72b4a8562b
bradyDarren <dcgbfinancial@gmail.com>
2024-12-31 20:04:51 -0600

    addition of the constructor method for the snake class as well as the create_snake method

--- snake.py
+++ snake.py
@@ -1,2 +1,24 @@
+from turtle import Turtle
 
-# class Snake: 
+class Snake: 
+    def __init__(self):
+        self.segments = []
+        self.initial_xcord = 0
+        self.initital_ycord = 0
+    
+    def create_snake(self):
+        for segment in range(3):
+            segment = Turtle(shape="square")
+            segment.penup()
+            segment.color("white")
+            segment.goto(x=self.initial_xcord, y=self.initital_ycord)
+            print(segment.position())            
+            self.segments.append(segment)
+            if len(self.segments) != 3:
+                self.initial_xcord -= 20
+
+    
+            
+
+
+
