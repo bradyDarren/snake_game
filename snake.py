16259ffdf3906d99c1d6a68261799775fe3d5a23
bradyDarren <dcgbfinancial@gmail.com>
2025-01-01 06:46:16 -0600

    addition of tehe add_segment method

--- snake.py
+++ snake.py
@@ -17,6 +17,27 @@ class Snake:
             if len(self.segments) != 3:
                 self.initial_xcord -= 20
 
+    def add_segment(self): 
+        additional_segment = Turtle(shape="square")
+        additional_segment.penup()
+        additional_segment.color("white")
+        x_cor = self.segments[len(self.segments) - 1].xcor()
+        y_cor = (self.segments[len(self.segments)-1].ycor())
+        if self.segments[0].heading() == 90 or self.segments[0] == 270:
+            y_cor = y_cor + 20
+            additional_segment.goto(x=x_cor, y=y_cor)
+            self.segments.append(additional_segment)
+        else: 
+            x_cor = x_cor + 20
+            additional_segment.goto(x=x_cor, y=y_cor)
+            self.segments.append(additional_segment)
+
+
+snake = Snake() 
+snake.create_snake()
+snake.add_segment()
+
+
     
             
 
