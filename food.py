f585abbe26c96c9b0402efff759e5521c4184393
bradyDarren <dcgbfinancial@gmail.com>
2025-01-01 16:47:36 -0600

    addition of code needed to randomly generate food. This code has not been formatted for OOP

--- food.py
+++ food.py
@@ -1,2 +1,8 @@
-
-# class Food: 
+class Food: 
+    def generate_food():
+    food_xcord = random.randint(-600,600)
+    food_ycord = random.randint(-475,475)
+    food = Turtle(shape="circle")
+    food.penup()
+    food.color("blue")
+    food.teleport(food_xcord,food_ycord) 
