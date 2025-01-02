1ed7cb90102c4d89cf3a652cb3494881cf862508
bradyDarren <dcgbfinancial@gmail.com>
2025-01-02 07:06:22 -0600

    addition of constructor mmethod and generate_food methods as wel.

--- food.py
+++ food.py
@@ -1,8 +1,25 @@
-class Food: 
-    def generate_food():
-    food_xcord = random.randint(-600,600)
-    food_ycord = random.randint(-475,475)
-    food = Turtle(shape="circle")
-    food.penup()
-    food.color("blue")
-    food.teleport(food_xcord,food_ycord) 
+from turtle import Turtle
+import random
+
+
+class Food(Turtle): 
+    def __init__(self):
+        super().__init__(shape="circle") 
+        self.penup() 
+        self.color("blue", "blue")
+        self.food_xlocation = []
+        self.food_ylocation = []
+        self.generate_food()
+
+    def generate_food(self):
+        for xaxis in range(-560,580,20):
+            self.food_xlocation.append(xaxis)
+        for yaxis in range(-460,480,20):
+            self.food_ylocation.append(yaxis)
+        
+        xcor = random.choice(self.food_xlocation)
+        ycor = random.choice(self.food_ylocation)
+        self.teleport(x=xcor,y=ycor)
+
+food = Food()
+        
